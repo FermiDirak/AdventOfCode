b5867c53cf4f41afa308c00677d30751aa67130e
Bryan Manuele <bryan@flexport.com>
2019-12-16 15:36:47 -0800

    yeet

--- day16-2.ts
+++ day16-2.ts
@@ -1,11 +1,12 @@
 import {readFileSync} from 'fs';
 
-const inputs = readFileSync('./day16-2.txt', 'utf8').split('\n');
-const offset = Number.parseInt(inputs[0]);
-// const frequencies = inputs[1].split('').map(input => Number.parseInt(input));
-const frequencies = '03036732577212944063491565474664'.split('').map(input => Number.parseInt(input));
+const inputs = readFileSync('./day16-1.txt', 'utf8').split('\n');
+const frequencies = inputs[0].split('').map(input => Number.parseInt(input));
+const offset = Number.parseInt(frequencies.slice(0, 7).join(''));
 
-const basePattern = [0, 1, 0, -1];
+function getOnesDigit(num: number) {
+  return Math.floor(Math.abs(num) % 10);
+}
 
 function dupInput(input: number[], count: number) {
   const res = [];
@@ -17,68 +18,34 @@ function dupInput(input: number[], count: number) {
   return res;
 }
 
-function getOnesDigit(num: number) {
-  return Math.abs(num) % 10;
-}
-
-function getComputedRepeatingPattern(repeat: number, index: number) {
-  const patternLength = repeat * 4;
-  const patternIndex = index % patternLength;
-
-  if (patternIndex === patternLength - 1) {
-    return basePattern[0];
-  } else if (patternIndex < (repeat * 1) - 1) {
-    return basePattern[0];
-  } else if (patternIndex < (repeat * 2) - 1) {
-    return basePattern[1];
-  } else if (patternIndex < (repeat * 3) - 1) {
-    return basePattern[2];
-  } else if (patternIndex < (repeat * 4) - 1) {
-    return basePattern[3];
-  }
-
-  throw new Error('wtf');
-}
-
-function computeForCell(inputs: number[], index: number) {
-  let sum = 0;
-
-  for (let i = 0; i < inputs.length; ++i) {
-    sum += inputs[i] * getComputedRepeatingPattern(index + 1, i);
-  }
-
-  return getOnesDigit(sum);
-}
-
-async function phase(inputs: number[]) {
+function phase(inputs: number[]) {
   const res = [];
+  let runningSum = 0;
 
-  for (let i = 0; i < inputs.length; ++i) {
-    const resolver = (resolve) => {
-      const result = computeForCell(inputs, i);
-      resolve(result);
-    }
+  for (let i = inputs.length - 1; i >= 0; --i) {
+    runningSum += inputs[i];
 
-    res.push(new Promise(resolver));
+    res.push(getOnesDigit(runningSum));
   }
 
-  return Promise.all(res);
+  return res.reverse();
 }
 
 function run() {
   const startTime = Date.now();
 
-  let currFreq = dupInput(frequencies, 500);
+  const rightHalfFrequencies = dupInput(frequencies, 10000).slice(offset);
+
+  let currFreq = rightHalfFrequencies;
 
   for (let i = 0; i < 100; ++i) {
-    process.stdout.write(`p${i}`)
     currFreq = phase(currFreq);
   }
 
   const endTime = Date.now();
 
   const result = currFreq.join('');
-  console.log(100, endTime - startTime, result.includes('74527'), )
+  console.log(100, endTime - startTime, result.substr(0, 8));
 }
 
 run();
