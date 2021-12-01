9c6f2a75639dea7dda4c61133c9de768600e5f36
bryan <manuelebryan@gmail.com>
2021-11-30 20:33:57 -0800

    set up this year's languages

---
+++ 2021/py_boilerplate.py
@@ -0,0 +1,4 @@
+# nodemon --exec "python3 ./2021/py_boilerplate.py" ./2021/py_boilerplate.py
+
+a = 3
+print(a)
---
+++ 2021/ts_boilerplate.ts
@@ -0,0 +1,4 @@
+// nodemon --exec "deno run --no-check ./2021/ts_boilerplate.ts" ./2021/ts_boilerplate.ts
+
+const a: string = 'a';
+console.log(a);
